3aadae6314cd52af3c00cecd078d4e85103bec6c
Balasubramanian1999 <absmani1999@gmail.com>
2023-03-25 09:10:16 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,19 @@
+// array 
+var n=[12,343,454,46];
+
+	document.write(n);
+	document.write("<br>");
+	document.write(n[2]);
+	document.write("<br>");
+	document.write(n[3]=5);
+	document.write("<br>");
+  n.push(100);
+	document.write(n);
+	document.write("<br>");
+	document.write("<br>");
+//for loop
+document.write("<h2>For loop</h2>")
+for (a=0;a<n.length;a++) {
+	document.write(n[a]);
+	document.write("<br>");
+}
